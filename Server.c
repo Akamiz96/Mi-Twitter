ef2be1f0ff35a341fa76c9bb7e7f8aa5c3810a68
Pablo Ariza Luna <pablo_ariza@javeriana.edu.co>
2017-05-12 15:54:45 -0500

    enviar tweet por servidor

--- Server.c
+++ Server.c
@@ -61,13 +61,11 @@ void registrar(int N, Cliente clientes[], EnvioCliente mensaje_cliente)
   else
     mensaje_server.respuesta = INVALIDO;
   if(write(clientes[aux.id - 1].pipe_id, &mensaje_server, sizeof(EnvioServer)) == -1)
-  {
     perror("En escritura");
-    exit(1);
-  }
 }
 
-void follow(int N, Cliente clientes[], int grafo[TAMUSR][TAMUSR], EnvioCliente mensaje_cliente)
+void follow(int N, Cliente clientes[], int grafo[TAMUSR][TAMUSR],
+            EnvioCliente mensaje_cliente)
 {
   int id_cliente;
   Cliente aux = (mensaje_cliente.cliente);
@@ -91,13 +89,11 @@ void follow(int N, Cliente clientes[], int grafo[TAMUSR][TAMUSR], EnvioCliente m
   else
     mensaje_server.respuesta = INVALIDO;
   if(write(clientes[aux.id - 1].pipe_id, &mensaje_server, sizeof(EnvioServer)) == -1)
-  {
     perror("En escritura");
-    exit(1);
-  }
 }
 
-void unfollow(int N, Cliente clientes[], int grafo[TAMUSR][TAMUSR], EnvioCliente mensaje_cliente)
+void unfollow(int N, Cliente clientes[], int grafo[TAMUSR][TAMUSR],
+              EnvioCliente mensaje_cliente)
 {
   int id_cliente;
   Cliente aux = (mensaje_cliente.cliente);
@@ -121,10 +117,71 @@ void unfollow(int N, Cliente clientes[], int grafo[TAMUSR][TAMUSR], EnvioCliente
   else
     mensaje_server.respuesta = INVALIDO;
   if(write(clientes[aux.id - 1].pipe_id, &mensaje_server, sizeof(EnvioServer)) == -1)
-  {
     perror("En escritura");
-    exit(1);
+}
+
+void tweet(int N, Cliente clientes[], int grafo[TAMUSR][TAMUSR], Respuesta modo,
+           EnvioCliente mensaje_cliente)
+{
+  int i, num_tweets;
+  Cliente aux = (mensaje_cliente.cliente);
+  EnvioServer mensaje_server;
+  FILE* file;
+  char archivo_tweet[MAXCHAR];
+
+  if(aux.id <= N && aux.id >= 1 )
+  {
+    for(i = 0; i < N; i++)
+    {
+      mensaje_server.respuesta = TWEET;
+      mensaje_server.tweet = mensaje_cliente.tweet;
+      if(grafo[i][aux.id - 1] == 1)
+      {
+        if(modo == ASINCRONO)
+        {
+          if(clientes[i].id != -1 )
+            if(write(clientes[aux.id - 1].pipe_id, &mensaje_server, sizeof(EnvioServer)) == -1)
+              perror("En escritura");
+            else
+              kill(aux.pid, SIGUSR1);
+          else
+          {
+            strcpy(archivo_tweet, "./tweet_pendientes/cliente_");
+            strcat(archivo_tweet, aux.id + ".dat\0");
+            file = fopen(archivo_tweet, "+rb");
+            if(file == NULL)
+              printf("Error al abrir el archivo %s\n", archivo_tweet);
+            else
+            {
+              if(fread(&num_tweets, sizeof(int), 1, file) == 0)
+                num_tweets = 0;
+              else
+                num_tweets++;
+              fwrite(&num_tweets, sizeof(int), 1, file);
+              fclose(file);
+              file = fopen(archivo_tweet, "ab");
+              fwrite(&mensaje_server, sizeof(EnvioServer), 1, file);
+            }
+          }
+        }
+      }
+      else
+      {
+        strcpy(archivo_tweet, "./tweet_pendientes/cliente_");
+        strcat(archivo_tweet, aux.id + ".dat\0");
+        file = fopen(archivo_tweet, "ab");
+        if(file == NULL)
+          printf("Error al abrir el archivo %s\n", archivo_tweet);
+        else
+        {
+          fwrite(&mensaje_server, sizeof(EnvioServer), 1, file);
+          kill(aux.pid, SIGUSR2);
+        }
+      }
+    }
   }
+  else
+    mensaje_server.respuesta = INVALIDO;
 }
 
 int main (int argc, char **argv)
@@ -137,14 +194,21 @@ int main (int argc, char **argv)
   Cliente clientes[TAMUSR];
   EnvioCliente mensaje_cliente;
   EnvioServer mensaje_server;
+  Respuesta modo;
 
   mode_t fifo_mode = S_IRUSR | S_IWUSR;
   if(argc != 5)
   {
-    printf("Error\nDebe ser ejecutado de la forma: %s N relaciones modo pipeNom\n", argv[0]);
+    printf("Error\nDebe ser ejecutado de la forma: %s N relaciones modo pipeNom\n",
+           argv[0]);
     exit(1);
   }
   N = atoi(argv[1]);
+  if(N < 1)
+  {
+    printf("Numero de clientes invalido debe ser mayor a 0\n");
+    exit(1);
+  }
   for(i = 0; i < N; i++)
   {
     clientes[i].id = -1;
@@ -162,6 +226,16 @@ int main (int argc, char **argv)
       grafo[i][j] = buffer[j * 2] == '1';
   }
   fclose ( file );
+  if(strcasecmp(argv[3],"sincrono"))
+    modo = SINCRONO;
+  else
+    if(strcasecmp(argv[3],"asincrono"))
+      modo = ASINCRONO;
+    else
+    {
+      printf("Modo invalido debe ser sincrono o asincrono\n");
+      exit(1);
+    }
   // Creacion del pipe inicial, el que se recibe como argumento del main
   unlink(argv[4]);
   if (mkfifo (argv[4], fifo_mode) == -1) {
@@ -194,8 +268,6 @@ int main (int argc, char **argv)
       case RE_TWEETS:
         //TODO Desconexion
         break;
-      default:
-        printf("Opcion Invalida\nIntente de nuevo");
     }
     //TODO mandar señal
   }
